ff6275635ea871ad849cc7168b37479ed08e448c
luissds82 <luis_sds82@berkeley.edu>
2015-10-20 18:32:21 -0700

    aux functions done

--- lab_6/solution.py
+++ lab_6/solution.py
@@ -6,6 +6,7 @@ import numpy
 import solid
 
 # EDITED - Luis #
+
 # USEFULL THING #
 
 def pts_to_vec (pt_a, pt_b):
@@ -14,19 +15,19 @@ def pts_to_vec (pt_a, pt_b):
   vec = end - start
   return vec
 
-################
+###################################
 
-# Importing a mechanism
+# IMPORTING A MECHANISM #
 gripper_r_arm = SynthFourBar(B = 35+18.52j, D = 84.15 + 52.93j, P= (100+237.1j, 76.95+244.45j, 53+250j, 28.6+253.3j, 4+254.73j)) #the solution is gripper_r_arm.children[0]
 gripper_arm = gripper
-
 
 robot_leg = SynthFourBar(B= 0+80j, D= 0+20j, P= pattern(0.5, 10+0j)) #the solution is robot_leg.children[1]
 robot_leg = robot_leg.children[1]
 
+###################################
 
-# Test
 
+# AUXILIAR FUNCTION #
 # Selecting the ternary link #
 # Making a filter function #
 # Test: which body has 3 joints? #
@@ -39,32 +40,49 @@ def filter_three_joints (a_mechanism):
 			pass
 	return ternary_link
 
-"""
+###################################
+
+
+# GETTING JOINT COORDINATES AND MODIFIED BOUNDING BOX (always a square for simplification issues) CORNER COORDINATES #
+
 def joint_coord (mechanism):
 	joints_coord=[]
 	bb_coord=[]
+	origin = numpy.array([0,0,0])
 	
 	ternary_body = filter_three_joints(mechanism)
 	ternary_joints = ternary_body.joints
 	for i in range(len(ternary_joints)):
 		joints_coord.append(ternary_joints[i].pose[0])
 	
-	BBox_points= mechanism.boundingBox().points
-	width= numpy.linalg.norm(pts_to_vec(BBox_points[0],BBox_points[3]))
+	BBox = ternary_body.bounding_box()
+	BBox_points= BBox.points
+	BBox_point0 = numpy.append(BBox_points[0], [0]) #numpy append to add a z coord
+
+	width= numpy.linalg.norm(pts_to_vec(BBox_points[0],BBox_points[2]))
 	height= numpy.linalg.norm(pts_to_vec(BBox_points[0],BBox_points[1]))
 	
 	if width == height:
-		bb_coord.extend((BBox_points[0], BBox_points[2]))
+		bb_square = BBox
 	elif width > height:
-		bb_square= Polyline(generator= solid.square(width)).simplified()
+		bb_square = PolyLine(generator = solid.square(width)).simplified()
+		tr_vec = translation_matrix(pts_to_vec(origin, BBox_point0))
+		bb_square *= tr_vec
 	else:
-		pass
+		bb_square = PolyLine(generator = solid.square(height)).simplified()
+		tr_vec = translation_matrix(pts_to_vec(origin, BBox_point0))
+		bb_square *= tr_vec
 
-	#length= 
+	bb_square_points = [numpy.append(bb_square.points[0], [0]), numpy.append(bb_square.points[2], [0])] #numpy append to add a z coord - consistency issues
 
-	return joints_coord, bb_coord
-"""
+	# For the sake of clarity transforming the arrays of bb_square_points to tuplles #
+	# In this way the output of the function would be two lists of tupples #
 
+	bb_pts_tpl= (map (tuple, bb_square_points))
+
+	return joints_coord, bb_pts_tpl
+
+###################################
 
 # Applying the joint_coord function to robot_leg #
 
