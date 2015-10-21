d15e32ce5ea3b29baef9d29771e7d92abba084a5
Casey Rogers <caseycrogers@berkeley.edu>
2015-10-20 22:17:10 -0700

    FINISHED?!

--- lab_6/solution.py
+++ lab_6/solution.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 from digifab import *
-from solution_lab5 import * #importing the SynthFourBar class
+#from solution_lab5 import * #importing the SynthFourBar class
 import numpy
 import solid
 
@@ -14,17 +14,6 @@ def pts_to_vec (pt_a, pt_b):
   end = numpy.asarray(pt_b)
   vec = end - start
   return vec
-
-###################################
-
-# IMPORTING MECHANISMS #
-gripper_r_arm = SynthFourBar(B = 35+18.52j, D = 84.15 + 52.93j, P= (100+237.1j, 76.95+244.45j, 53+250j, 28.6+253.3j, 4+254.73j)) #the solution is gripper_r_arm.children[0]
-gripper_arm = gripper_r_arm.children[0]
-
-robot_leg = SynthFourBar(B= 0+80j, D= 0+20j, P= pattern(0.5, 10+0j)) #the solution is robot_leg.children[1]
-robot_leg = robot_leg.children[1]
-
-###################################
 
 
 # AUXILIAR FUNCTION #
@@ -119,18 +108,31 @@ def dummy_arm(name):
 def dumb_coordinates(cList, bounds, dim):
   bound0 = bounds[0]
   bound1 = bounds[1]
- 	ret = []
- 	dim = dim + 1
- 	for c in cList:
- 		gX = numpy.floor( [(c[0]-bound0[0])/(bound1[1]-bound0[0])*dim] )[0]
- 		gY = numpy.floor( [(c[1]-bound0[1])/(bound1[1]-bound0[1])*dim] )[0]
- 		ret.append(dim*(gx + 1) + (dim-gY))
- 	return ret
+  ret = []
+  dim = dim + 1
+  for c in cList:
+    gX = numpy.round( (c[0]-bound0[0])/(bound1[1]-bound0[0])*dim )
+    gY = numpy.round( (c[1]-bound0[1])/(bound1[1]-bound0[1])*dim )
+    ret.append(dim*(gX) + gY)
+  return ret
 
 # EDITED - Casey ##################
 
 if __name__ == '__main__':
+
+  ###################################
+
+  # IMPORTING MECHANISMS #
+  #gripper_r_arm = SynthFourBar(B = 35+18.52j, D = 84.15 + 52.93j, P= (100+237.1j, 76.95+244.45j, 53+250j, 28.6+253.3j, 4+254.73j)) #the solution is gripper_r_arm.children[0]
+  #gripper_arm = gripper_r_arm.children[0]
+
+  #robot_leg = SynthFourBar(B= 0+80j, D= 0+20j, P= pattern(0.5, 10+0j)) #the solution is robot_leg.children[1]
+  #robot_leg = robot_leg.children[1]
+
+  ###################################
+
   p = PolyLine(filename='ternary_link_020.png')
   p.show()
-  bot_leg_joints_bb = joints_bb_coord(robot_leg)
-  print dumb_coordinates(bot_leg_joints_bb[0], bot_leg_joints_bb[1], 60)
+  #bot_leg_joints_bb = joints_bb_coord(robot_leg)
+  #print bot_leg_joints_bb[0]
+  #print dumb_coordinates(bot_leg_joints_bb[0], bot_leg_joints_bb[1], 60)
