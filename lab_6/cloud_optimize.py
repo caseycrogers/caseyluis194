6260c2c835efc61c18ade857d4f40ef59b6eac43
Casey Rogers <caseycrogers@berkeley.edu>
2015-10-22 20:38:17 -0700

    added neighbors

--- lab_6/cloud_optimize.py
+++ lab_6/cloud_optimize.py
@@ -2,13 +2,13 @@ from digifab import *
 import math
 import numpy
 
-cloud_optimize(cloud, edges):
+def cloud_optimize(cloud, edges):
   while(optimize(cloud, edges)):
     pass
   return
 
 # Attempts ten times to improve the worst point in the cloud
-optimize(cloud, edges):
+def optimize(cloud, edges):
   # find the bounding box of the sculpture
 
   # finish
@@ -34,24 +34,35 @@ optimize(cloud, edges):
   return False
 
 # Returns a number for goodness of fit given a point, higher = better
-hPoint(cloud, edges, index):
+def hPoint(cloud, edges, index):
+
   return number
 
 # Returns a number for goodness of fit for the cloud, higher = better
-hCloud(cloud, edges):
+def hCloud(cloud, edges):
   return number
 
 # Finds the point with the most acute diahedral angle
-findWorstPoint(cloud, edges):
+def findWorstPoint(cloud, edges):
   return worst point
 
 
 # Moves the point within radius sphere randomly
-randomMove(pt, radius, bounds):
+def randomMove(pt, radius, bounds):
   # make sure new point isn't too close to any other point
   # make sure new point isn't out of bounds
   # try ten times, if failure, return Null
   return original position of point
+
+# Find the points a given point is connected to
+def neighbors(edges, index):
+  n = []
+  for edge in edges:
+    if edge[0] == index:
+      n.append(edge[1])
+    elif edge[1] == index:
+      n.append(edge[0])
+  return n
 
 # Find the diahedral angle given two vectors
 
