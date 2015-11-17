003b6895e8924267fffe3be52f6fa9b5472422e4
Casey Rogers <caseycrogers@berkeley.edu>
2015-11-17 14:49:48 -0800

    commitaroo

--- Final_Project/heliodon_maker.py
+++ Final_Project/heliodon_maker.py
@@ -20,7 +20,7 @@ def hJoint (thick, width, border, innerD, bolt, t, right):
     (solid.cylinder(r=outerD/2,center=True, h=width, segments = 20))
   j = solid.translate(v = [0, width/2, outerD/2])(j)
   j = j + solid.cube([outerD,width,border])
-  j = j + solid.translate([0, 0, border+thick+2*t])\
+  j = j + solid.translate([0, 0, border+thick])\
     (solid.cube([outerD,width,border]))
   j = j - solid.translate(v = [0, 0, border])(solid.cube([outerD,width,thick+2*t]))
 
@@ -42,19 +42,32 @@ def hJoint (thick, width, border, innerD, bolt, t, right):
       (solid.cylinder(r=bolt/2, h = outerD, segments = 20)))
 
   j = solid.translate(v=[0, 0, -border])(j)
-  return PolyMesh(generator=j)
+  if right:
+    j = solid.translate(v=[width, 0, 0])(solid.rotate(a=[0, 0, 90])(j))
+  return (PolyMesh(generator=j), outerD)
 
-def arc(thick, width, radius):
+def arc(thick, width, radius, outerD, bolt):
   a = solid.difference()(
-    solid.cylinder(r=radius), solid.cylinder(r=radius-width))
+    solid.cylinder(r=radius, h=thick), solid.cylinder(r=radius-width, h=thick))
   a = solid.intersection()(a, solid.cube([radius, radius, thick]))
+
+  a = solid.difference()(a, 
+    solid.translate(v=[.75*outerD, 90+width/2, 0])
+      (solid.cylinder(r=bolt/2, h = outerD, segments = 20)))
+  a = solid.difference()(a, 
+    solid.translate(v=[90+width/2, .75*outerD, 0])
+      (solid.cylinder(r=bolt/2, h = outerD, segments = 20)))
+
   return PolyMesh(generator=a)
 
-jR = hJoint(3, 10, 3, 3, 3.5, .2, True)
-jL = hJoint(3, 10, 3, 3, 3.5, .2, False)
+jR, outerD = hJoint(3, 10, 3, 3, 3.5, .2, True)
+jL, outerD = hJoint(3, 10, 3, 3, 3.5, .2, False)
 jR.save("rightjoint.stl")
 jL.save("leftjoint.stl")
-a100 = arc(3, 10, 100)
+a100 = arc(3, 10, 100, outerD, 3.5)
+
+PolyLine(generator = solid.projection()(a100.get_generator())).save("a100.dxf")
+
 jR *= translation_matrix([90,0,0])
 jL *= translation_matrix([0,90,0])
 a100 |= jR
